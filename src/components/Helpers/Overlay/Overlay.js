d3b3c5e5b7fff84774ff1f66a43d58919ecf830c
savawedev <andrei.sava.dev@gmail.com>
2022-05-23 21:39:29 +0300

    Added hero background

---
+++ src/components/Helpers/Overlay/Overlay.js
@@ -0,0 +1,7 @@
+import styles from "./Overlay.module.css";
+
+const Overlay = () => {
+  return <div className={styles.overlay}></div>;
+};
+
+export default Overlay;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,12 @@
+import Overlay from "../Helpers/Overlay/Overlay";
 import styles from "./Hero.module.css";
 
 const Hero = () => {
-  return <section></section>;
+  return (
+    <section className={styles.hero}>
+      <Overlay />
+    </section>
+  );
 };
 
 export default Hero;
