3c1756b1ccb733e4e4579be0102155ab2f160499
savawedev <andrei.sava.dev@gmail.com>
2022-05-23 21:19:13 +0300

    Styled Title component for the header

--- src/components/Header/Title/Title.js
+++ src/components/Header/Title/Title.js
@@ -1,7 +1,11 @@
 import styles from "./Title.module.css";
 
 const Title = () => {
-  return <span>Movie World</span>;
+  return (
+    <span className={styles.title}>
+      Movie <span className={styles["title-highlight"]}>World</span>
+    </span>
+  );
 };
 
 export default Title;
