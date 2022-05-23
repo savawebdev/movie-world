87d8dcbfa064fe9b57c2d1524568267abc6f0495
savawedev <andrei.sava.dev@gmail.com>
2022-05-23 21:48:01 +0300

    Added hero Title component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,12 @@
 import Overlay from "../Helpers/Overlay/Overlay";
+import Title from "./Title/Title";
 import styles from "./Hero.module.css";
 
 const Hero = () => {
   return (
     <section className={styles.hero}>
       <Overlay />
+      <Title />
     </section>
   );
 };
