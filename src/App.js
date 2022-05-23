c08378cebfe5d6d0245f42907048e749ce9ba555
savawedev <andrei.sava.dev@gmail.com>
2022-05-23 21:34:28 +0300

    Started home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 };
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,7 @@
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+  return <section></section>;
+};
+
+export default Hero;
